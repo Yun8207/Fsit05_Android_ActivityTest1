1882fee0bc82905be79fe6b45b650706e83d355a
Alex <alexs880121@gmail.com>
2018-10-07 10:35:20 +0800

    v1

--- app/src/main/java/tw/alex/activitytest1/MainActivity.java
+++ app/src/main/java/tw/alex/activitytest1/MainActivity.java
@@ -1,13 +1,63 @@
 package tw.alex.activitytest1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public MainActivity(){
+        Log.d("alex", "MainActivity");
+    }
+
+    public void test1(View view){
+        Intent intent = new Intent(this, Page2Activity.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("alex","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("alex","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("alex","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("alex","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("alex","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("alex","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("alex","onRestart");
+
     }
 }
---
+++ app/src/main/java/tw/alex/activitytest1/Page2Activity.java
@@ -0,0 +1,18 @@
+package tw.alex.activitytest1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Page2Activity extends AppCompatActivity {
+    public void test2(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+}
